d10edb7e6d61f82df7b5f669cd1a0b895e39fc3c
Anton Lyashkevich <anton_lyashkevich@epam.com>
2023-04-22 01:17:30 +0300

    js-cloud-dev-5: feat(task-5): - Integration with S3 - Update api path for import file.

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -28,21 +28,26 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
 
     if (!file) return;
     // Get the presigned URL
-    const response = await axios({
-      method: "GET",
-      url,
-      params: {
-        name: encodeURIComponent(file.name),
-      },
-    });
-    console.log("File to upload: ", file.name);
-    console.log("Uploading to: ", response.data);
-    const result = await fetch(response.data, {
-      method: "PUT",
-      body: file,
-    });
-    console.log("Result: ", result);
-    setFile(undefined);
+    try {
+      const response = await axios({
+        method: "GET",
+        url,
+        params: {
+          name: encodeURIComponent(file.name),
+        },
+      });
+      console.log("File to upload: ", file.name);
+      console.log("Uploading to: ", response.data.payload);
+      const result = await fetch(response.data.payload, {
+        method: "PUT",
+        body: file,
+      });
+      console.log("Result: ", result);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setFile(undefined);
+    }
   };
   return (
     <Box>
